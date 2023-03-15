77970ce02acded47a581e9baecd5140c93471fc6
PZadric <pzadri00@fesb.hr>
2023-03-15 18:57:16 +0100

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pzadri1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
